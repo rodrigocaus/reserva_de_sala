5101ef381ec23dbb0b979f5878d7ccee14b76f5a
thales.teixeira1996@gmail.com <owelha@Thales-DELL.localdomain>
2019-07-01 19:44:37 +0000

    Alterei a ordem de uns testes

--- spec/api/login_spec.js
+++ spec/api/login_spec.js
@@ -13,7 +13,19 @@ const Joi = frisby.Joi; // Frisby exposes Joi for convenience
 
 describe('test suite', function () {
 
-	it('testa login', function (t1) {
+	it('testa a cadastro de aluno', function (t1) {
+		frisby.put('http://localhost:3000/login', {
+			nome: "Thales",
+			matricula: "158445",
+			senha: "123",
+			senhaconfirma: "123"
+		})
+			.expect('status', 200)
+			.expect('bodyContains', 'Usuario inserido')
+			.done(t1);
+	});
+
+	it('testa login', function (t2) {
 		frisby.post('http://localhost:3000/login', {
 			matricula: "158445",
 			senha: '123'
@@ -21,38 +33,24 @@ describe('test suite', function () {
 			.expect('status', 200)
 			.expect('header', 'Set-Cookie', new RegExp('[userAuth].*'))
 			.expect('bodyContains', 'Login efetuado')
-			.done(t1);
+			.done(t2);
 	});
 
 
-	it('testa login falho', function (t2) {
+	it('testa login falho', function (t3) {
 		frisby.post('http://localhost:3000/login', {
 			matricula: "158445",
 			senha: '153'
 		})
 			.expect('status', 401)
 			.expect('bodyContains', 'Falha no login')
-			.done(t2);
-	});
-
-
-	it('testa a cadastro de aluno', function (t3) {
-		frisby.put('http://localhost:3000/login', {
-			nome: "Thales",
-			matricula: "741845",
-			senha: "753951",
-			senhaconfirma: "753951"
-		})
-			.expect('status', 200)
-			.expect('bodyContains', 'Usuario inserido')
 			.done(t3);
 	});
-
 
 	it('testa a cadastro de aluno repetido', function (t4) {
 		frisby.put('http://localhost:3000/login', {
 			nome: "Thales",
-			matricula: "741852",
+			matricula: "158445",
 			senha: "753951",
 			senhaconfirma: "753951"
 		})
@@ -60,7 +58,6 @@ describe('test suite', function () {
 			.expect('bodyContains', 'Usuario ja cadastrado')
 			.done(t4);
 	});
-
 
 	it('testa logout', function (t5) {
 		frisby.delete('http://localhost:3000/login')
